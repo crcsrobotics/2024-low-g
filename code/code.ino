3a3b19d9cebe78af508daef8cf7a31264ba0207e
Djtpj <93952263+Djtpj@users.noreply.github.com>
2024-11-11 22:09:04 -0600

    Autonomous Task + more

--- code/code.ino
+++ code/code.ino
@@ -11,13 +11,19 @@ unsigned long debounceDelay = 50;
 
 std::map<int, Button> buttonMap;
 
-const bool DEBUG_MODE = true;
+const int MOTOR_CLOCKWISE = 180, MOTOR_COUNTERCLOCKWISE = 0, MOTOR_STATIONARY = 90;
 
 Servo driveLeft, driveRight;
 Servo hipMotor, rakeMotor;
 
 Servo safetyBarServo;
 Servo mmmFloorLeft, mmmFloorRight;
+
+bool isAutonomous = false;
+
+int rakePot = GIZMO_ADC_1;
+int collisionSwitch = GIZMO_GPIO_1;
+
 
 // The Gizmo provides access to the data that is held by the field
 // management system and the gizmo system processor.
@@ -50,6 +56,9 @@ void setup() {
   pinMode(GIZMO_SERVO_2, OUTPUT);
   pinMode(GIZMO_SERVO_3, OUTPUT);
 
+  pinMode(rakePot, INPUT);
+  pinMode(collisionSwitch, INPUT);
+
   buttonInit();
   
   driveLeft.attach(GIZMO_MOTOR_1);
@@ -76,38 +85,64 @@ void loop() {
   // Refreshes the information about axis and button state.
   gizmo.refresh();
 
-  drive();
+  if (buttonJustPressed(GIZMO_BUTTON_START)) {
+    isAutonomous = true;
+  }
 
-  if (buttonJustPressed(GIZMO_BUTTON_A))
-    servoToggle(safetyBarServo);
+  if (buttonJustPressed(GIZMO_BUTTON_BACK)) {
+    isAutonomous = false;
+  }
 
+  if (isAutonomous) {
+    autonomousTask();
+    return;
+  }
+
+  driveLogic();
+  
+  // this is right next to the drive logic in order to override the drive commands sent if you are pressing the joysticks.
+  if (gizmo.getAxis(GIZMO_AXIS_DY) == 254) {
+    robotBackward(1);
+  }
+
+  Serial.println("Rake Pot:" + String(analogRead(rakePot)));
+  Serial.println("Collision Switch:" + String(digitalRead(collisionSwitch)));
+
+  // Floor logic
   if (buttonJustPressed(GIZMO_BUTTON_B)) {
-    servoToggle(mmmFloorLeft);
-    servoToggle(mmmFloorRight, 0, 180);
+    servoToggle(mmmFloorLeft, 0, 180);
+    servoToggle(mmmFloorRight, 180, 0);
   }
 
   if (gizmo.getButton(GIZMO_BUTTON_LSHOULDER)) {
-    hipMotor.write(0);
+    // Full speed couterclockwwise
+    hipMotor.write(MOTOR_COUNTERCLOCKWISE);
   } else if (gizmo.getButton(GIZMO_BUTTON_LT)) {
-    hipMotor.write(180);
+    // Full speed clockwise
+    hipMotor.write(MOTOR_CLOCKWISE);
   } else {
-    hipMotor.write(90);
+    // No speed
+    hipMotor.write(MOTOR_STATIONARY);
   }
 
   if (gizmo.getButton(GIZMO_BUTTON_RSHOULDER)) {
-    rakeMotor.write(0);
+    // Full speed counterclockwise
+    rakeMotor.write(MOTOR_COUNTERCLOCKWISE);
    } else if (gizmo.getButton(GIZMO_BUTTON_RT)) {
-    rakeMotor.write(180);
+    // Full speed clockwise
+    rakeMotor.write(MOTOR_CLOCKWISE);
   } else {
-    rakeMotor.write(90);
+    // No speed
+    rakeMotor.write(MOTOR_STATIONARY);
   }
 
   buttonUpdate();
 
+  // Limit tick speed to something less crazy. Just makes the math easier
   delay(20);
 }
 
-void drive() {
+void driveLogic() {
   // Fetch the speed of each axis, then convert this to the range
   // expected by the motors.
   int targetL, targetR;
@@ -119,15 +154,81 @@ void drive() {
   driveRight.write(targetR);
 }
 
-void servoToggle(Servo servo, int min, int max) {
+void servoToggle(Servo &servo, int min, int max) {
   int target = servo.read() == max ? min : max;
-  Serial.println(servo.read());
-  Serial.println(target);
+
   servo.write(target);
 }
 
-void servoToggle(Servo servo) {
-  servoToggle(servo, 0, 180);
+void autonomousTask() {
+  // pause for dramatic effect 💅
+  delay(1000);
+
+  // Raise rake to preliminary angle
+  moveMotor(45, rakeMotor, rakePot);
+
+  // accounting for duration of motion
+  delay(1000);
+
+  // drive until wall
+  while(digitalRead(collisionSwitch) != HIGH) {
+    robotForward(1);
+
+    // allows for task cancelling
+    if (gizmo.getButton(GIZMO_BUTTON_BACK)) {
+      isAutonomous = false;
+      return;
+    }
+  }
+
+  // lower rake all the way
+  moveMotor(90, rakeMotor, rakePot);
+
+  const float BACKUP_SPEED = 0.25;
+  robotBackward(BACKUP_SPEED);
+
+  // time for a backup at top speed
+  const float DRIVE_TIME_SECONDS = .9;
+  // time for a backup at set speed
+  const float DRIVE_TIME_ADJUSTED = DRIVE_TIME_SECONDS * (1/BACKUP_SPEED);
+
+  delay(DRIVE_TIME_ADJUSTED * 1000);
+
+  robotStop();
+
+  moveMotor(0, rakeMotor, rakePot);
+
+  isAutonomous = false;
+}
+
+void robotForward(float speed) {
+  driveLeft.write(MOTOR_CLOCKWISE * speed);
+  driveRight.write(MOTOR_COUNTERCLOCKWISE * speed);
+}
+
+void robotBackward(float speed) {
+  driveLeft.write(MOTOR_CLOCKWISE * speed);
+  driveRight.write(MOTOR_CLOCKWISE * speed);
+}
+
+void robotStop() {
+  driveLeft.write(MOTOR_STATIONARY);
+  driveRight.write(MOTOR_STATIONARY);
+}
+
+void moveMotor(int posDegrees, Servo &motor, int pot) {
+  // Convert the potentiometer position to degrees
+  int position = map(analogRead(pot), 0, 1024, 0, 180);
+
+  while (position != posDegrees) {
+    if (position < posDegrees) {
+      motor.write(MOTOR_CLOCKWISE);
+    } else {
+      motor.write(MOTOR_COUNTERCLOCKWISE);
+    }
+  }
+
+  motor.write(MOTOR_STATIONARY);
 }
 
 void buttonInit() {
@@ -162,6 +263,7 @@ void buttonUpdate() {
 }
 
 bool buttonJustPressed(int index) {
+  // saves a little bit of time, but mostly this was useful for debugging
   if (gizmo.getButton(index) == 0) return false;
 
   Button button = buttonMap[index];
